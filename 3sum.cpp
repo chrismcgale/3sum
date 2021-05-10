2aa075d3b060fe41c1717b5a3f07ee8efac29ba9
Christopher McGale <56483395+chrismcgale@users.noreply.github.com>
2021-05-10 17:06:11 -0400

    Update 3sum.cpp

--- 3sum.cpp
+++ 3sum.cpp
@@ -5,18 +5,20 @@ using std::vector;
 using std::cin;
 using std::cout;
 
+//O(nlogn) sorting algo
 void insertSort(vector<int> &A, int l, int r)
 {
 	for(int i = l; i <= r; i++){
 		int j = i;
 		while( (j > 0) && (A[j] < A[j - 1])){
-            std::swap(A[j], A[j-1]);
+            		std::swap(A[j], A[j-1]);
 			j--;
 		}
 	}
 }
 
-std::pair<int, int> two_sum(vector<int> arr, int c) {
+std::pair<int, int> two_sum(vector<int> arr, int c) 
+{
     int L = 0;
     int R = arr.size() - 1;
     while (L <= R) {
@@ -29,19 +31,24 @@ std::pair<int, int> two_sum(vector<int> arr, int c) {
     return std::make_pair(0, 0);
 }
 
-void three_sum(vector<int> arr, int tar) {
+
+void three_sum(vector<int> arr, int tar)
+{
     insertSort(arr, 0, arr.size() - 1);
+    //For easch element of the array check whether 2 elements sum to target - element
     for(int i = 0; i < arr.size() - 1; ++i) {
-        std::pair<int, int> ret = two_sum(arr, tar - arr[i]);
+        std::pair<int, int> ret = two_sum(arr, tar - arr[i]);    
         if (ret != std::pair<int, int>(0, 0)) {
             cout << "Found at i=" << i << " j=" << ret.first << " k=" << ret.second << std::endl;
             return;
-        }
+        }    
     }
     cout << "Not found" << std::endl;
 }
 
-int main() {
+
+int main() 
+{
     vector<int> arr;
     int i;
     int tar;
